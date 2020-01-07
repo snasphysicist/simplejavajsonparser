91953188fd0ff129478731e230167e2331c5aa6f
snasphysicist <ih8coldplay@hotmail.com>
2020-01-07 21:02:26 +0800

    Outline of first test

---
+++ src/test/java/simplejsonparser/TestJSONString.java
@@ -0,0 +1,13 @@
+
+package simplejsonparser;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestJSONString {
+
+    @Test
+    void givenTextBetweenQuotationMarksShouldParseSuccessfully() {
+        
+    }
+}
