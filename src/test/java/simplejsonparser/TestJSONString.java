5170ee03ce7b3ca51e12fba96b1485bce9ba03b5
snasphysicist <ih8coldplay@hotmail.com>
2020-01-07 21:06:07 +0800

    Sanity check that a properly written string in a JSON object can be parsed

--- src/test/java/simplejsonparser/TestJSONString.java
+++ src/test/java/simplejsonparser/TestJSONString.java
@@ -8,6 +8,9 @@ class TestJSONString {
 
     @Test
     void givenTextBetweenQuotationMarksShouldParseSuccessfully() {
-        
+        JSONString jsonString = new JSONString();
+        jsonString.parseFrom("\"Test string\"");
+        assertTrue(jsonString.success());
+        assertEquals("Test string", jsonString.getValue());
     }
 }
