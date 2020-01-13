11029564897c306047754948cc07a83d9ccdf888
snasphysicist <ih8coldplay@hotmail.com>
2020-01-13 20:21:40 +0800

    Sanity check that JSON with integer value parsed successfully

--- src/test/java/simplejsonparser/TestJSONObject.java
+++ src/test/java/simplejsonparser/TestJSONObject.java
@@ -20,4 +20,18 @@ public class TestJSONObject {
         assertTrue(parsed.get("key") instanceof JSONString);
     }
 
+    @Test
+    public void givenValidJSONWithIntegerValueShouldParseSuccessfully() {
+        String jsonString = "{\"key\":1}";
+        JSONObject jsonObject = new JSONObject();
+        String remainder = jsonObject.parseFrom(jsonString);
+        assertEquals("", remainder);
+        assertTrue(jsonObject.success());
+        HashMap<String, JSONElement> parsed = jsonObject.getObject();
+        assertNotNull(parsed.get("key"));
+        assertTrue(parsed.get("key") instanceof JSONNumber);
+        Integer value = ((JSONNumber) parsed.get("key")).castToInteger();
+        assertEquals(1, value);
+    }
+
 }
