af296ac255f7995265a1360713484ac4a6344cac
snasphysicist <ih8coldplay@hotmail.com>
2020-01-13 21:23:04 +0800

    Debug test code

--- src/main/java/simplejsonparser/SimpleJSONParser.java
+++ src/main/java/simplejsonparser/SimpleJSONParser.java
@@ -4,7 +4,8 @@ package simplejsonparser;
 public class SimpleJSONParser {
 
     public static void main(String[] args) {
-        
+        JSONString jsonString = new JSONString();
+        System.out.println(jsonString.parseFrom("\"A\"\""));
     }
 
 }
