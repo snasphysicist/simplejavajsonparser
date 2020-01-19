636041b24c68575eabb3698257685adcf6301e60
snasphysicist <ih8coldplay@hotmail.com>
2020-01-19 14:46:53 +0800

    Methods to remove the trailing JSON whitespace from a string

--- src/main/java/simplejsonparser/StringManipulation.java
+++ src/main/java/simplejsonparser/StringManipulation.java
@@ -16,6 +16,21 @@ class StringManipulation {
         return stripped;
     }
 
+    static String stripTrailingJSONWhitespace(String from) {
+        String stripped = from;
+        while (
+                stripped.length() > 0
+                        && isJsonWhitespace(
+                                stripped.charAt(
+                                        stripped.length() - 1
+                                )
+                        )
+        ) {
+            stripped = removeLastCharacter(stripped);
+        }
+        return stripped;
+    }
+
     private static boolean isJsonWhitespace(char character) {
         for (char whitespace : JSON_WHITESPACE) {
             if (whitespace == character) {
@@ -27,6 +42,10 @@ class StringManipulation {
 
     static String removeFirstCharacter(String from) {
         return from.substring(1);
+    }
+
+    static String removeLastCharacter(String from) {
+        return from.substring(0, from.length() - 1);
     }
 
 }
