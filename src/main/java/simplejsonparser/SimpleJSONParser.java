68a71eea7ecfd803059a2c304f618cf6a0cbd0fe
snasphysicist <ih8coldplay@hotmail.com>
2020-01-19 14:47:38 +0800

    Debugging code from JSONNumber parsing issue

--- src/main/java/simplejsonparser/SimpleJSONParser.java
+++ src/main/java/simplejsonparser/SimpleJSONParser.java
@@ -5,7 +5,10 @@ public class SimpleJSONParser {
 
     public static void main(String[] args) {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.parseFrom("{\"A\":\"B\"");
+        jsonObject.parseFrom("{\"key\":  [ \n\r  10\t \t, \" \r\n\t \"  \r\r\t  ,\n true  \t\r  ]}");
+        JSONElement[] elements = ((JSONArray) jsonObject.getObject().get("key")).getElements();
+        JSONNumber number = ((JSONNumber) elements[0]);
+        System.out.println(number.castToInteger());
     }
 
 }
