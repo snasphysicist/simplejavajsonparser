defbd4732cd6eb5e5e72fb855adfa9ed0388afa1
snasphysicist <ih8coldplay@hotmail.com>
2020-01-19 14:47:58 +0800

    Sanity check that an array can be parsed when there is extra JSON whitespace

--- src/test/java/simplejsonparser/TestJSONArray.java
+++ src/test/java/simplejsonparser/TestJSONArray.java
@@ -34,4 +34,30 @@ class TestJSONArray {
         assertEquals(BOOLEAN_VALUE, ((JSONBoolean) elements[2]).getValue());
     }
 
+    @Test
+    void givenArrayWithExtraWhitespaceAtStartOfJSONStringShouldParseSuccessfully() {
+        String whitespaceString = String.format(
+                " \n \r %s  \t  ",
+                STRING_VALUE
+        );
+        String jsonString = String.format(
+                "  [ \n\r  %d\t \t, \"%s\"  \r\r\t  ,\n %b  \t\r  ]}",
+                INTEGER_VALUE,
+                whitespaceString,
+                BOOLEAN_VALUE
+        );
+        JSONArray jsonArray = new JSONArray();
+        String remainder = jsonArray.parseFrom(jsonString);
+        assertTrue(jsonArray.success());
+        assertEquals("}", remainder);
+        JSONElement[] elements = jsonArray.getElements();
+        assertEquals(3, elements.length);
+        assertTrue(elements[0] instanceof JSONNumber);
+        assertTrue(elements[1] instanceof JSONString);
+        assertTrue(elements[2] instanceof JSONBoolean);
+        System.out.println(elements[0]);
+        assertEquals(INTEGER_VALUE, ((JSONNumber) elements[0]).castToInteger());
+        assertEquals(whitespaceString, ((JSONString) elements[1]).getValue());
+        assertEquals(BOOLEAN_VALUE, ((JSONBoolean) elements[2]).getValue());
+    }
 }
